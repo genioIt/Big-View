f386d3345ad19a8410d8e6a6bbe33750ccd2d54e
genioIt <42850755+genioIt@users.noreply.github.com>
2025-08-08 17:41:19 -0500

    Add files via upload

---
+++ Backend/WebApiEComerce.Application/App/OrdersApplication.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiEcommerce.Application.Entity;
+using WebApiEcommerce.Application.Interface;
+using WebApiEcommerce.Application.Model;
+using WebApiEcommerce.Domain.Common;
+using WebApiEcommerce.Domain.Entity;
+using WebApiEcommerce.Domain.Interface;
+
+namespace WebApiEcommerce.Application.App
+{
+    public class OrdersApplication : IOrdersApplication
+    {
+        private readonly IOrderRepository _OrderRepository;
+        private IMapper _mapper;
+
+        public OrdersApplication(IOrderRepository orderRepository, IMapper mapper)
+        {
+            _OrderRepository = orderRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<ResponseServiceDTO> AddAsync(OrdersDTO entity)
+        {
+            try
+            {
+                Orders ordersMap = _mapper.Map<OrdersDTO, Orders>(entity);
+                Orders Orders = await _OrderRepository.AddAsync(ordersMap);
+
+                return new ResponseServiceDTO
+                {
+                    Success = true,
+                    Message = "Se ha agregado la orden del pedido",
+                    Tipo = "Success"
+                };
+            }
+            catch (InvalidOrderAmountException ex)
+            {
+                return new ResponseServiceDTO
+                {
+                    Success = true,
+                    Message = ex.Message,
+                    Tipo = "BusinessError"
+                };
+            }
+            catch (Exception ex)
+            {
+                return new ResponseServiceDTO
+                {
+                    Success = true,
+                    Message = ex.Message,
+                    Tipo = "BusinessError"
+                };
+            }
+        }
+    }
+}
---
+++ Backend/WebApiEComerce.Application/App/UserRolesApplication.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiEcommerce.Application.Entity;
+using WebApiEcommerce.Application.Model;
+using WebApiEcommerce.Domain.Entity;
+using WebApiEcommerce.Domain.Interface;
+
+namespace WebApiEcommerce.Application.App
+{
+    public class UserRolesApplication
+    {
+        private readonly IUserRolesRepository _UserRolesRepository;
+        private IMapper _mapper;
+
+        public UserRolesApplication(IUserRolesRepository userRolesRepository, IMapper mapper)
+        {
+            _UserRolesRepository = userRolesRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<ResponseServiceDTO> CreateUserRolesAsync(UserRolesDTO entity)
+        {
+             try
+            {
+                var userRoles = _mapper.Map<UserRolesDTO,UserRoles>(entity);
+                await _UserRolesRepository.AddAsync(userRoles);
+                return new ResponseServiceDTO
+                {
+                    Success = true,
+                    Message = "User role added successfully.",
+                    Tipo = "Success"
+                };
+            }
+            catch (Exception ex)
+            {
+                return new ResponseServiceDTO
+                {
+                    Success = false,
+                    Message = ex.Message,
+                    Tipo = "Error"
+                };
+            }
+        }
+    }
+}
